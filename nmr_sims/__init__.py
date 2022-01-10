824e923f4e9f38c3c10c473f1b82f68cc28fa489
5hulse <simonhulse@protonmail.com>
2022-01-10 18:07:34 +0000

    Updated SpinSystem.

--- nmr_sims/__init__.py
+++ nmr_sims/__init__.py
@@ -1,12 +1,12 @@
 # __init__.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Mon 10 Jan 2022 15:19:42 GMT
+# Last Edited: Mon 10 Jan 2022 18:02:28 GMT
 
 from __future__ import annotations
 from pathlib import Path
 import re
-from typing import Any, Union
+from typing import Any, Tuple, Union
 import numpy as np
 import numpy.linalg as nlinalg
 import scipy.linalg as slinalg
@@ -246,3 +246,112 @@ class CartesianBasis:
                 operator = operator.kroenecker(self._E)
 
         return 2 ** (self.nspins - 1) * operator
+
+
+class SpinSystem(CartesianBasis):
+    # TODO Assuming spin-1/2 at the moment.
+    def __init__(self, spins: dict) -> None:
+        self.shifts, self.couplings = self._process_spins_dict(spins)
+        super().__init__(I=0.5, nspins=self.shifts.size)
+
+    @staticmethod
+    def _process_spins_dict(spins: Any) -> Tuple[np.ndarray, np.ndarray]:
+        if not isinstance(spins, dict):
+            raise TypeError("`spins` should be a dict.")
+
+        spin_keys = list(spins.keys())
+        nspins = len(spin_keys)
+        if not all([isinstance(key, int) for key in spin_keys]):
+            raise ValueError(
+                "`spins` should solely have ints as keys."
+            )
+
+        if not sorted(spin_keys) == list(range(1, nspins + 1)):
+            raise ValueError(
+                "The keys in `spins` should be consecutive ints, starting at 1."
+            )
+
+        shifts = np.zeros(nspins)
+        couplings = np.zeros((nspins, nspins))
+
+        for i, spin in spins.items():
+            if not all([k in spin for k in ("shift", "couplings")]):
+                raise ValueError(
+                    "Each value in `spins` should be a dict with the keys "
+                    "\"shift\" and \"couplings\". "
+                    f"This is not satisfied by spin {i}."
+                )
+
+            if not isinstance(spin["shift"], (int, float)):
+                raise TypeError(
+                    "\"shift\" entries should be scalar values. This is not "
+                    f"satisfied by spin {i}."
+                )
+
+            shifts[i - 1] = spin["shift"]
+
+            if not isinstance(spin["couplings"], dict):
+                raise TypeError(
+                    "\"couplings\" entries should be dicts. This is not satisfied by "
+                    f"spin {i}."
+                )
+
+            for j, coupling in spin["couplings"].items():
+                if not (isinstance(j, int) and (1 <= j <= nspins) and (j != i)):
+                    raise ValueError(
+                        f"Invalid key in couplings dict for spin {i}: {j}. Should "
+                        f"be and int greater than 0, no more than {nspins}, and not "
+                        f"{i}."
+                    )
+
+                current_value = couplings[i - 1, j - 1]
+                if float(current_value) != 0.:
+                    if coupling != current_value:
+                        raise ValueError(
+                            f"Contradictory couplings given between spins {j} and "
+                            f"{i}: {float(coupling)} and {current_value}."
+                        )
+                else:
+                    couplings[i - 1, j - 1] = coupling
+                    couplings[j - 1, i - 1] = coupling
+
+        return shifts, couplings
+
+    def edit_shift(self, spin: int, value: Union[int, float]) -> None:
+        self._check_valid_spin(spin)
+        if not isinstance(value, (int, float)):
+            raise TypeError("`value` should be a scalar value.")
+        self.shifts[spin - 1] = value
+
+    def edit_coupling(self, spin1: int, spin2: int, value: Union[int, float]) -> None:
+        self._check_valid_spin(spin1)
+        self._check_valid_spin(spin2)
+        if spin1 == spin2:
+            raise ValueError("`spin1` and `spin2` cannot match.")
+
+        if not isinstance(value, (int, float)):
+            raise TypeError("`value` should be a scalar value.")
+
+        self.couplings[spin1 - 1, spin2 - 1] = value
+        self.couplings[spin2 - 1, spin1 - 1] = value
+
+    def _check_valid_spin(self, spin: Any) -> None:
+        if not (isinstance(spin, int) and 1 <= spin <= self.nspins):
+            raise ValueError(
+                "`spin` should be an int greater than 0 and no more than "
+                f"{self.nspins}"
+            )
+
+    @property
+    def free_hamiltonian(self):
+        H = Operator(np.zeros((self.dim, self.dim)))
+        for i in range(self.nspins):
+            H += 2 * np.pi * self.shifts[i] * self.get(f"{i + 1}z")
+            for j in range(i + 1, self.nspins):
+                H += 2 * np.pi * self.couplings[i, j] * (
+                    self.get(f"{i + 1}x{j + 1}x") +
+                    self.get(f"{i + 1}y{j + 1}y") +
+                    self.get(f"{i + 1}z{j + 1}z")
+                )
+
+        return H
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,11 @@
 # test.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Mon 10 Jan 2022 15:20:23 GMT
+# Last Edited: Mon 10 Jan 2022 17:53:23 GMT
 
 import pytest
 import numpy as np
-from nmr_sims import CartesianBasis, Operator
+from nmr_sims import CartesianBasis, Operator, SpinSystem
 
 
 sq2 = np.sqrt(2)
@@ -67,6 +67,7 @@ E1Z2 = Operator(np.array(
         [0, 0, 0, -0.5],
     ]
 ))
+
 
 class TestOperator:
     def test_init(self):
@@ -232,3 +233,126 @@ class TestCartesianBasis:
         assert basis.get("1x2y") == X1Y2
         assert basis.get("1z") == Z1E2
         assert basis.get("2z") == E1Z2
+
+
+class TestSpinSystem:
+
+    @staticmethod
+    def get_spins():
+        return {
+            1: {
+                "shift": 1000,
+                "couplings": {
+                    2: 40,
+                },
+            },
+            2: {
+                "shift": 500,
+                "couplings": {
+                    1: 40,
+                },
+            },
+        }
+
+    def test_keys_not_ints(self):
+        # Key is a str, when all should be ints
+        spins = self.get_spins()
+        spins["1"] = spins[1]
+        del spins[1]
+        with pytest.raises(ValueError) as exc_info:
+            SpinSystem(spins)
+        assert "`spins` should solely have ints as keys." == str(exc_info.value)
+
+    def test_nonconsecutive_int_keys(self):
+        # spins dict has labels 2, 3. Not valid as no 1!
+        spins = self.get_spins()
+        spins[3] = spins[1]
+        del spins[1]
+        with pytest.raises(ValueError) as exc_info:
+            SpinSystem(spins)
+        assert (
+            "The keys in `spins` should be consecutive ints, starting at 1." ==
+            str(exc_info.value)
+        )
+
+    def test_invalid_spin_dict(self):
+        # spin 1 does not have a shift defined
+        spins = self.get_spins()
+        del spins[1]["shift"]
+        with pytest.raises(ValueError) as exc_info:
+            SpinSystem(spins)
+        assert (
+            "\"shift\" and \"couplings\". This is not satisfied by spin 1." in
+            str(exc_info.value)
+        )
+
+    def test_invalid_shift(self):
+        # spin 2's shift is given by a string.
+        spins = self.get_spins()
+        spins[2]["shift"] = "40"
+        with pytest.raises(TypeError) as exc_info:
+            SpinSystem(spins)
+        assert (
+            "should be scalar values. This is not satisfied by spin 2." in
+            str(exc_info.value)
+        )
+
+    def test_invalid_coupling_key(self):
+        spins = self.get_spins()
+        spins[1]["couplings"][1] = spins[1]["couplings"][2]
+        del spins[1]["couplings"][2]
+        with pytest.raises(ValueError) as exc_info:
+            SpinSystem(spins)
+        errmsg = str(exc_info.value)
+        assert "spin 1: 1." in errmsg
+        assert "no more than 2, and not 1." in errmsg
+
+        spins = self.get_spins()
+        spins[1]["couplings"][3] = spins[1]["couplings"][2]
+        with pytest.raises(ValueError) as exc_info:
+            SpinSystem(spins)
+        errmsg = str(exc_info.value)
+        assert "spin 1: 3." in errmsg
+        assert "no more than 2, and not 1." in errmsg
+
+    def test_contradictionary_couplings(self):
+        spins = self.get_spins()
+        spins[1]["couplings"][2] = 20
+        with pytest.raises(ValueError) as exc_info:
+            SpinSystem(spins)
+        assert "between spins 1 and 2: 40.0 and 20.0." in str(exc_info.value)
+
+    def test_init(self):
+        spins = self.get_spins()
+        spin_system = SpinSystem(spins)
+        assert spin_system.nspins == 2
+        assert np.array_equal(spin_system.shifts, np.array([1000, 500]))
+        assert np.array_equal(spin_system.couplings, np.array([[0, 40], [40, 0]]))
+        assert spin_system.get("1x2y") == X1Y2
+
+    def test_edit_shift(self):
+        spin_system = SpinSystem(self.get_spins())
+        spin_system.edit_shift(1, -500)
+        spin_system.edit_shift(2, 600)
+        assert np.array_equal(spin_system.shifts, np.array([-500, 600]))
+
+        with pytest.raises(ValueError) as exc_info:
+            spin_system.edit_shift(3, 1000)
+        assert "greater than 0 and no more than 2" in str(exc_info.value)
+
+        with pytest.raises(TypeError) as exc_info:
+            spin_system.edit_shift(1, "-500")
+        assert str(exc_info.value) == "`value` should be a scalar value."
+
+    def test_edit_copuling(self):
+        spin_system = SpinSystem(self.get_spins())
+        spin_system.edit_coupling(1, 2, 60)
+        assert np.array_equal(spin_system.couplings, np.array([[0, 60], [60, 0]]))
+
+        with pytest.raises(ValueError) as exc_info:
+            spin_system.edit_coupling(1, 1, 60)
+        assert str(exc_info.value) == "`spin1` and `spin2` cannot match."
+
+    def test_free_hamiltonian(self):
+        spin_system = SpinSystem(self.get_spins())
+        print(spin_system.free_hamiltonian)
